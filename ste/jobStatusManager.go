e164c8e3d9e8227e2fe336896b5354034eaf12b4
Narasimha Kulkarni <63087328+nakulkar-msft@users.noreply.github.com>
2021-03-02 09:34:06 +0530

    Replace goroutine by mutex

--- ste/jobStatusManager.go
+++ ste/jobStatusManager.go
@@ -22,6 +22,7 @@ package ste
 
 import (
 	"time"
+	"sync"
 
 	"github.com/Azure/azure-storage-azcopy/common"
 )
@@ -36,69 +37,64 @@ type jobPartCreatedMsg struct {
 
 type xferDoneMsg = common.TransferDetail
 type jobStatusManager struct {
+	m           sync.Mutex
 	js          common.ListJobSummaryResponse
-	respChan    chan common.ListJobSummaryResponse
-	listReq     chan bool
-	partCreated chan jobPartCreatedMsg
-	xferDone    chan xferDoneMsg
 }
+
 
 var jstm jobStatusManager
 
 /* These functions should not fail */
 func (jm *jobMgr) SendJobPartCreatedMsg(msg jobPartCreatedMsg) {
-	jstm.partCreated <- msg
+	js := &jstm.js
+	jstm.m.Lock()
+	defer jstm.m.Unlock()
+
+	js.CompleteJobOrdered = js.CompleteJobOrdered || msg.isFinalPart
+	js.TotalTransfers += msg.totalTransfers
+	js.FileTransfers += msg.fileTransfers
+	js.FolderPropertyTransfers += msg.folderTransfer
+	js.TotalBytesEnumerated += msg.totalBytesEnumerated
+	js.TotalBytesExpected += msg.totalBytesEnumerated
 }
 
 func (jm *jobMgr) SendXferDoneMsg(msg xferDoneMsg) {
-	jstm.xferDone <- msg
+	js := &jstm.js
+	jstm.m.Lock()
+	defer jstm.m.Unlock()
+
+	switch msg.TransferStatus {
+		case common.ETransferStatus.Success():
+			js.TransfersCompleted++
+			js.TotalBytesTransferred += msg.TransferSize
+		case common.ETransferStatus.Failed(),
+			common.ETransferStatus.TierAvailabilityCheckFailure(),
+			common.ETransferStatus.BlobTierFailure():
+			js.TransfersFailed++
+			js.FailedTransfers = append(js.FailedTransfers, common.TransferDetail(msg))
+		case common.ETransferStatus.SkippedEntityAlreadyExists(),
+			common.ETransferStatus.SkippedBlobHasSnapshots():
+			js.TransfersSkipped++
+			js.SkippedTransfers = append(js.SkippedTransfers, common.TransferDetail(msg))
+	}
 }
 
 func (jm *jobMgr) ListJobSummary() common.ListJobSummaryResponse {
-	jstm.listReq <- true
-	return <-jstm.respChan
-}
-
-func (jm *jobMgr) ResurrectSummary(js common.ListJobSummaryResponse) {
-	jstm.js = js
-}
-
-func (jm *jobMgr) handleStatusUpdateMessage() {
 	js := &jstm.js
+	jstm.m.Lock()
+	defer jstm.m.Unlock()
+
+	/* Display stats */
+	js.Timestamp = time.Now().UTC()
 	js.JobID = jm.jobID
 	js.CompleteJobOrdered = false
 	js.ErrorMsg = ""
+	return *js
+}
 
-	for {
-		select {
-		case msg := <-jstm.partCreated:
-			js.CompleteJobOrdered = js.CompleteJobOrdered || msg.isFinalPart
-			js.TotalTransfers += msg.totalTransfers
-			js.FileTransfers += msg.fileTransfers
-			js.FolderPropertyTransfers += msg.folderTransfer
-			js.TotalBytesEnumerated += msg.totalBytesEnumerated
-			js.TotalBytesExpected += msg.totalBytesEnumerated
+func (jm *jobMgr) ResurrectSummary(js common.ListJobSummaryResponse) {
+	jstm.m.Lock()
+	defer jstm.m.Unlock()
 
-		case msg := <-jstm.xferDone:
-			switch msg.TransferStatus {
-			case common.ETransferStatus.Success():
-				js.TransfersCompleted++
-				js.TotalBytesTransferred += msg.TransferSize
-			case common.ETransferStatus.Failed(),
-				common.ETransferStatus.TierAvailabilityCheckFailure(),
-				common.ETransferStatus.BlobTierFailure():
-				js.TransfersFailed++
-				js.FailedTransfers = append(js.FailedTransfers, common.TransferDetail(msg))
-			case common.ETransferStatus.SkippedEntityAlreadyExists(),
-				common.ETransferStatus.SkippedBlobHasSnapshots():
-				js.TransfersSkipped++
-				js.SkippedTransfers = append(js.SkippedTransfers, common.TransferDetail(msg))
-			}
-
-		case <-jstm.listReq:
-			/* Display stats */
-			js.Timestamp = time.Now().UTC()
-			jstm.respChan <- *js
-		}
-	}
+	jstm.js = js
 }
